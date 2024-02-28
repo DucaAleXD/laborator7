bf8cb102b69445bba006aa40db1525f3af171449
DucaAleXD <ducasasha@gmail.com>
2024-02-28 08:35:04 +0200

    Compledarea codului pentru interfata in welcome.blade.php

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\Subscribe;
 
 class SubscribeController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscribeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('subscribe',[SubscribeController::class,'store'])->name('subscribe');
