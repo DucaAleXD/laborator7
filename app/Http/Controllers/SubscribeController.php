81b341df132d4c1605841751c2401e031f19da51
DucaAleXD <ducasasha@gmail.com>
2024-02-28 08:26:07 +0200

    Configurare migrare cu date

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class SubscribeController extends Controller
 {
-    //
+    //Functie 
+    public function store(Request $request){
+        Subscribe::create($request->all());
+        return redirect()->back()->with('success','Inregistrarea reusita!');
+    }
 }
